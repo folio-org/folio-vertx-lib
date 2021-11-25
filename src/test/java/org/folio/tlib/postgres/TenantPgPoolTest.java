fdfe917acc246bf35e773bbdc007e4282233b544
Adam Dickmeiss <adam@indexdata.dk>
2021-11-25 17:11:46 +0100

    Testing

--- src/test/java/org/folio/tlib/postgres/TenantPgPoolTest.java
+++ src/test/java/org/folio/tlib/postgres/TenantPgPoolTest.java
@@ -8,6 +8,7 @@ import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.sqlclient.PrepareOptions;
 import io.vertx.sqlclient.SqlConnection;
 import io.vertx.sqlclient.Tuple;
 import java.io.IOException;
@@ -120,15 +121,15 @@ public class TenantPgPoolTest {
     withPool(pool -> pool
         .query("SELECT count(*) FROM pg_database")
         .execute())
-    .onComplete(context.asyncAssertSuccess());
+        .onComplete(context.asyncAssertSuccess());
   }
 
   @Test
-  public void preparedQueryOk(TestContext context) {
+  public void preparedQueryOptionsOk(TestContext context) {
     withPool(pool -> pool
-        .preparedQuery("SELECT * FROM pg_database WHERE datname=$1")
+        .preparedQuery("SELECT * FROM pg_database WHERE datname=$1", new PrepareOptions())
         .execute(Tuple.of("postgres")))
-    .onComplete(context.asyncAssertSuccess());
+        .onComplete(context.asyncAssertSuccess());
   }
 
   @Test
@@ -214,6 +215,24 @@ public class TenantPgPoolTest {
     .onComplete(context.asyncAssertSuccess(rowSet -> {
       assertThat(rowSet.iterator().next().getString(0), is("TLSv1.3"));
     }));
+  }
+
+  @Test
+  public void size() {
+    TenantPgPool pool = TenantPgPool.pool(vertx, "diku");
+    assertThat(pool.size(), is(0));
+  }
+
+  @Test
+  public void close(TestContext context) {
+    TenantPgPool pool = TenantPgPool.pool(vertx, "diku");
+    pool.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void connectHandler(TestContext context) {
+    TenantPgPool pool = TenantPgPool.pool(vertx, "diku");
+    pool.connectHandler(null);
   }
 
 }
