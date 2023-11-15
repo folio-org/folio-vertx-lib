abfb4ee4849dc9bf0e23a02a1fa9832836207be1
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-11-15 13:54:28 +0100

    Add TenantPgPoolContainer.create() test coverage

--- pg-testing/src/test/java/org/folio/tlib/postgres/testing/TenantPgPoolContainerTest.java
+++ pg-testing/src/test/java/org/folio/tlib/postgres/testing/TenantPgPoolContainerTest.java
@@ -6,11 +6,25 @@ import static org.hamcrest.Matchers.is;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import org.folio.tlib.postgres.TenantPgPool;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class TenantPgPoolContainerTest {
+
+  /**
+   * Assert
+   * <a href="https://github.com/testcontainers/testcontainers-java/blob/main/modules/postgresql/src/main/java/org/testcontainers/containers/PostgreSQLContainer.java">
+   * PostgreSQLContainer.java</a> default.
+   */
+  @Test
+  void defaultConnectOptions() {
+    assertThat(TenantPgPool.getDefaultConnectOptions().getDatabase(), is("db"));
+    TenantPgPoolContainer.create();
+    assertThat(TenantPgPool.getDefaultConnectOptions().getDatabase(), is("test"));
+  }
 
   @ParameterizedTest
   @MethodSource
