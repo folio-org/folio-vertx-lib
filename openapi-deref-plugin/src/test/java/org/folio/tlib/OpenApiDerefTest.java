252b8c0356f33ec0a4c7c8cb2e9a11b54844ee84
Adam Dickmeiss <adam@indexdata.dk>
2025-08-15 17:28:26 +0200

    More testing of plugin

--- openapi-deref-plugin/src/test/java/org/folio/tlib/OpenApiDerefTest.java
+++ openapi-deref-plugin/src/test/java/org/folio/tlib/OpenApiDerefTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class OpenApiDerefTest {
 
   @Test
-  void testDeref() throws Exception {
+  void testDerefYaml() throws Exception {
     String input = "src/test/resources/openapi/reftest.yaml";
     String output = "target/generated-resources/openapi/reftest.deref.yaml";
     OpenApiDeref.fix(input, output);
@@ -20,4 +20,35 @@ class OpenApiDerefTest {
     // check that $ref is not found anywhere
     assertFalse(outputContent.contains("$ref"));
   }
+
+  @Test
+  void testDerefJson() throws Exception {
+    String input = "src/test/resources/openapi/reftest.yaml";
+    String output = "target/generated-resources/openapi/reftest.deref.json";
+    OpenApiDeref.fix(input, output);
+    // read output file into memory
+    String outputContent = new String(Files.readAllBytes(Paths.get(output)), StandardCharsets.UTF_8);
+    assertNotNull(outputContent);
+    // check that $ref is not found anywhere
+    assertFalse(outputContent.contains("$ref"));
+  }
+
+  @Test
+  void testFailToParse() {
+    String input = "src/test/resources/openapi/headers/okapi-token.yaml";
+    String output = "target/generated-resources/openapi/okapi-token.deref.json";
+    assertThrows(java.io.IOException.class, () -> {
+      OpenApiDeref.fix(input, output);
+    });
+  }
+
+  @Test
+  void testNoFile() {
+    String input = "src/test/resources/openapi/headers/no-file-en.yaml";
+    String output = "target/generated-resources/openapi/no-file.deref.json";
+    assertThrows(java.io.IOException.class, () -> {
+      OpenApiDeref.fix(input, output);
+    });
+  }
+
 }
